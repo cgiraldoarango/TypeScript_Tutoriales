01b7f688438d5d127a842eec59deb70a0220fd5e
carlos giraldo <cgiraldoarango@gmail.com>
2023-05-04 20:33:08 -0500

    cambio con una funcion nueva

--- type/type_alias.ts
+++ type/type_alias.ts
@@ -1,4 +1,10 @@
+//Defiendo nuestro primer type
 type nombreUsuario = {
     nombre: string;
     edad: number
 }
+//crear una función para usar el alias de tipo
+function imprimirNombre(usuario: nombreUsuario) {
+    console.log(`El usuario se llama ${usuario.nombre}`)
+}
+imprimirNombre({ nombre: "Juan Pérez", edad: 40 })
